1d6bd8969581d9a42dccb8282ba1b4d96ef8a59b
Kadir シ︎ <119422088+kadirbeyz@users.noreply.github.com>
2022-12-04 12:24:31 +0300

    Add files via upload

---
+++ Arduino Mesafe Ölçme.ino
@@ -0,0 +1,39 @@
+//kadirbeyz
+//https://github.com/kadirbeyz
+
+#include <LiquidCrystal_I2C_AvrI2C.h>
+ 
+LiquidCrystal_I2C_AvrI2C lcd(0x27,16,2);  
+ 
+int trigPin = 13; 
+int echoPin = 12;  
+long zaman;
+long mesafe;
+ 
+void setup(){
+pinMode(trigPin, OUTPUT); 
+pinMode(echoPin,INPUT); 
+lcd.begin(); //lcd'yi başlatıyoruz
+lcd.backlight(); //lcd arka ışığını açıyoruz.
+ 
+}
+void loop(){
+digitalWrite(trigPin, LOW); 
+delayMicroseconds(5);
+digitalWrite(trigPin, HIGH); 
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);  
+zaman = pulseIn(echoPin, HIGH);
+mesafe= (zaman /29.1)/2;    
+if(mesafe<4){mesafe=4;}
+else if(mesafe>100){mesafe=100;}
+ 
+  lcd.clear();
+  lcd.setCursor(0,0); 
+  lcd.print("MESAFE OLCER"); 
+  lcd.setCursor(0,1); 
+  lcd.print(mesafe);  
+  lcd.setCursor(4,1); 
+  lcd.print("cm");    
+delay(500); 
+}
